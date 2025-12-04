7e0c7f0b29bbaf03fd2b7e86b34d78a4fd6836bf
bob0900 <bob.cheatham@gmail.com>
2025-12-04 08:07:55 -0500

    Add files via upload
    The PowerShell scripts create the needed json parameter file for SQL DB and SQL MI. There are a total of two scripts, one for each PaaS platform.

---
+++ SQLDB-only-DBWatcher.ps1
@@ -0,0 +1,77 @@
+﻿# Connect-AzAccount
+
+# Set your key vault name and secret names
+# $keyVaultName = "KV-Purview-Robertc"
+# $usernameSecretName = "SecretUserName"
+# $passwordSecretName = "dbPassword"
+# Get the username and password secrets
+
+
+# Parameters List
+    $dbwatchername = "DBWatcher-Robertc"
+    $region = "eastus"
+    $resourcegroupname = "SQL_Managed_Instance"
+    $kustoclustername = "sqldb-watcher"
+    $kustodatabasename = "dbwatcher"
+    
+#Set the values for the json files so that all SQL Managed Instances are listed
+
+
+$sqldbtargets = "{" + "`$schema" + ":" + """https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#"",
+  ""contentVersion"": ""1.0.0.0"","
+
+
+
+$sqldbtargets  = $sqldbtargets + "
+  ""parameters"": {
+    ""watcherName"": { ""value"":" +  "'" + $dbwatchername + "'" +  "},
+    ""location"": { ""value"": " + "'" + $region + "'" + " },
+    ""kustoResourceGroupName"": { ""value"":" + "'" + $resourcegroupname + "'" + " },
+    ""kustoClusterName"": { ""value"":" + "'" + $kustoclustername + "'" +  " },
+    ""kustoDatabaseName"": { ""value"":" + "'" +  $kustodatabasename + "'" + " }, "
+    
+  
+
+    $sqlServers = Get-AzSqlServer
+     foreach ($server in $sqlServers) {
+
+         
+        #Write-Host "  SQL Server: $($server.ServerName) - Resource Group: $($server.ResourceGroupName)"
+        # List SQL Databases in this server
+        $databases = Get-AzSqlDatabase -ServerName $server.ServerName -ResourceGroupName $server.ResourceGroupName | ?{$_.Edition -notlike "DataWarehouse" -and $_.DatabaseName -notlike "master" -and $_.Status -notlike "Paused" }
+        
+        $filtereddatabases = $databases
+
+#Input database values
+if ($databases.Count -ne 0) {
+ $sqldbtargets = $sqldbtargets + "      ""sqlTargets"": { ""value"": [ "
+}
+$index = 0
+      
+       foreach ($db in $filtereddatabases  ) {
+            #Write-Host "    SQL Database: $($db.DatabaseName) - Database Edition: $($db.Edition)"
+
+            
+       $sqldbtargets = $sqldbtargets + "                       
+                          { ""resourceGroupName"": ""$($db.ResourceGroupName)"",
+                            ""sqlServerName"": ""$($db.ServerName)"",
+                            ""databaseName"": ""$($db.DatabaseName)"",
+                            ""authenticationType"": ""Aad"",
+                            ""enablePrivateLink"": true,
+                            ""readIntent"": false }"
+                             $index++
+
+        if ($index -lt $filtereddatabases.Count)
+            {
+                $sqldbtargets = $sqldbtargets + ","
+                
+            }
+        }
+    
+if ($databases.Count -ne 0) {
+    $sqldbtargets = $sqldbtargets + "                 ] } } }"        
+}    
+    
+    }
+
+$sqldbtargets | Out-File -FilePath "C:\bicep\dbwatcher.parameters.json"
---
+++ SQLMI-only-DBWatcher.ps1
@@ -0,0 +1,73 @@
+﻿# Parameters List
+    $dbwatchername = "DBWatcher-Robertc"
+    $region = "eastus"
+    $resourcegroupname = "SQL_Managed_Instance"
+    $kustoclustername = "sqldb-watcher"
+    $kustodatabasename = "dbwatcher"
+    
+ 
+#Set the values for the json files so that all SQL Managed Instances are listed
+
+$sqlmitargets = "{" + "`$schema" + ":" + """https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#"",
+  ""contentVersion"": ""1.0.0.0"","
+
+
+$sqlmitargets  = $sqlmitargets + "
+  ""parameters"": {
+    ""watcherName"": { ""value"":" +  "'" + $dbwatchername + "'" +  "},
+    ""location"": { ""value"": " + "'" + $region + "'" + " },
+    ""kustoResourceGroupName"": { ""value"":" + "'" + $resourcegroupname + "'" + " },
+    ""kustoClusterName"": { ""value"":" + "'" + $kustoclustername + "'" +  " },
+    ""kustoDatabaseName"": { ""value"":" + "'" +  $kustodatabasename + "'" + " }, "
+
+ 
+ #Set the values for the json files so that all SQL Managed Instances are listed
+ $index = 0
+ 
+ $sqlmanagedinstancetargets =  " 
+  ""sqlManagedInstanceTargets"": { ""value"": [ "
+    
+    $managedInstances = Get-AzSqlInstance
+    $totalCount = $managedInstances.Count
+
+    foreach ($mi in $managedInstances) 
+    {
+ 
+ # Write-Host "  Managed Instance: $($mi.ManagedInstanceName) - Resource Group: $($mi.ResourceGroupName)"
+    
+ 
+ $sqlmanagedinstancetargets = $sqlmanagedinstancetargets + " 
+    { ""resourceGroupName"": ""$($mi.ResourceGroupName)"",
+      ""managedInstanceName"": ""$($mi.ManagedInstanceName)"",
+      ""authenticationType"": ""Aad"",
+      ""readIntent"": false } "
+      
+      $index++
+
+        if ($index -lt $managedInstances.Count)
+            {
+                $sqlmanagedinstancetargets = $sqlmanagedinstancetargets + ","
+                
+            }
+    
+    }
+ 
+
+ #Write-Host $sqlmanagedinstancetargets 
+  
+ 
+  # $jsonscript = " {
+  # parameters: {
+  #   watcherName: { value:" +  $dbwatchername + " },
+  #  location: { value:" +  $region + " },
+  #  kustoResourceGroupName: { value:" +  $resourcegroup + " },
+  #  kustoClusterName: { alue:" +  $kustoclustername + " },
+  #  kustoDatabaseName: { value:" +   $kustodatabasename + " }, } } "
+
+$sqlmanagedinstancetargets = $sqlmanagedinstancetargets + " ] } } } " 
+
+$jsonscript = $sqlmitargets + $sqlmanagedinstancetargets #$jsonscript + 
+
+Write-Host $jsonscript
+
+$jsonscript | Out-File -FilePath "C:\bicep\dbwatcher.parameters.json"
