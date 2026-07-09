37f5bcad5cf6661afbb11516e2c87f626e07ff61
bob0900 <bob.cheatham@gmail.com>
2026-07-09 10:07:26 -0400

    Add files via upload

--- 0 List Servers Only - SQLDB target Instances.ps1
+++ 0 List Servers Only - SQLDB target Instances.ps1
@@ -1,44 +1,72 @@
 ﻿$SQL_Command = ''
 #install-Module -Name Az.* -Force -AllowClobber
 #install-Module -Name SqlServer -Force -AllowClobber
-#install-module az.sql
-#Update-Module -Name Az.Accounts -Force -RequiredVersion 5.5.0
-# Install-Module az.accounts -MinimumVersion 5.5.0 -Force -AllowClobber
-# Import-Module Az.Accounts -RequiredVersion 5.5.0 -Force 
+#Update-Module -Name Az.Accounts -Force -RequiredVersion 5.5.1
+# update-Module az.accounts -requiredversion 5.5.1 -Force 
+# Import-Module Az.Accounts -RequiredVersion 5.5.1 -Force 
 # Install-module az.sql -RequiredVersion 7.0.0 -Force -AllowClobber
-# Import-Module az.sql -RequiredVersion 7.0.0 -force 
+# import-Module az.sql -RequiredVersion 7.0.0 -force 
 #uninstall-module az.accounts -Force 
-
-#update-Module -Name Az.accounts
+#Install-Module -Name Az.Accounts -RequiredVersion 5.3.3 -Force -AllowClobber
+#update-Module -Name Az.accounts -Force
 #Install-Module -Name Az -Repository PSGallery -Force
 
-#Install-Module -Name Az.Accounts -scope CurrentUser -RequiredVersion 5.3.3 -Force
 
-# Connect-AzAccount -devicecode
+#Connect-AzAccount -devicecode
 
-# Get the access token for Azure SQL Database
-#$accessToken = (Get-AzAccessToken -ResourceUrl https://database.windows.net).Token
+$DBWatcherName = "Must BE UPDATED"
+$MainServerName = "sqldb-maintenance-robertc.database.windows.net"
+$MaintDBName = "dbMaintenance"
+$DBName = ""
+$ServiceTier = ""
+$ComputeTier = ""
+$DeploymentModel = ""
+$AdminName = ""
+$ResourceGroupName = ""
+$LogicalSQLDB = ""
 
 
-$subsWithSqlServers = foreach ($sub in Get-AzSubscription) 
-            {
-  Set-AzContext -SubscriptionId $sub.Id | Out-Null
-  $servers = Get-AzSqlServer -ErrorAction SilentlyContinue
+#Get the access token for Azure SQL DB
+$accessToken = (Get-AzAccessToken -ResourceUrl "https://database.windows.net").Token
+
+
+$subsWithSqlServers = foreach ($sub in Get-AzSubscription) {
+
+Set-AzContext -SubscriptionId $sub.Id | Out-Null
   
- 
 $sqlServers = Get-AzSqlServer -ErrorAction SilentlyContinue
-     foreach ($server in $sqlServers) 
-  {
+
+foreach ($server in $sqlServers) {
 
         #Write-Host "  SQL Server: $($server.ServerName) - Resource Group: $($server.ResourceGroupName)"
         # List SQL Databases in this server
+        $ServerInstancename = $server.FullyQualifiedDomainName
 
-        $ServerInstancename = "$($server.ServerName).database.windows.net"
+#$LogicalSQLDB = Get-AzSqlDatabase -ResourceGroupName $server.ResourceGroupName -ServerName $server.ServerName |
+#        Where-Object { $_.DatabaseName -ne "master" } |
+#            Select-Object *, 
+#                  @{Name="DeploymentModel"; Expression={if($_.ElasticPoolName){"Elastic Pool ($($_.ElasticPoolName))"}else{"Single Database"}} 
 
-        #List all instances non-filtered
-        #Write-Host $ServerInstancename
-        #Write-Host " SubscriptionName: $($sub.Name)   Resource Group: $($server.ResourceGroupName)      DB-InstaneName - $($ServerInstancename)" 
-   
+
+Write-Host "--- Processing Server: $($server.ServerName) ---" -ForegroundColor Cyan
+
+$SQLDBs =  Get-AzSqlDatabase -ResourceGroupName $server.ResourceGroupName -ServerName $server.ServerName -ErrorAction SilentlyContinue |
+        Where-Object { $_.DatabaseName -ne "master" } |
+           Select-Object *, 
+                @{Name="DeploymentModel"; Expression={if($_.ElasticPoolName){"Elastic Pool ($($_.ElasticPoolName))"}else{"Single Database"}}} -ErrorAction SilentlyContinue
+    
+
+foreach ($LogicalSQLDB in $SQLDBs)   {
+
+        $ResourceGroupName = $server.ResourceGroupName 
+        $DBName = $LogicalSQLDB.DatabaseName
+        $ServiceTier = $LogicalSQLDB.Edition
+        $ComputeTier = $LogicalSQLDB.CurrentServiceObjectiveName
+        $DeploymentModel = $LogicalSQLDB.Deploymentmodel
+        $AdminName = $server.SqlAdministratorLogin
+
+
+Write-Host "    Found Database: $($LogicalSQLDB.DatabaseName)" -ForegroundColor Green 
    
 #Provides the ability to filter by Resource Groups such as test and prod  
 if ($server.ResourceGroupName -like "**") 
@@ -48,28 +76,32 @@ if ($server.ResourceGroupName -like "**")
                    -ServerName $server.ServerName `
                     -ErrorAction SilentlyContinue
 
+#Query used to insert target sqldb's to DBWatcher 
 
-#Provides the ability to filter by Admin group for the SQL DB instances
-     if ($admin -and $admin.ObjectId -ne "5fbc5476-2e5f-45b8-b3a8-2f3092f79d5c") 
-                                                                {
-            [PSCustomObject]@{
-                SubscriptionId = [string]$sub.Id
-                ServerName     = [string]$server.ServerName
-                ResourceGroup  = [string]$server.ResourceGroupName
-                AdminObjectId  = [string]$admin.ObjectId
-                AdminName      = [string]$admin.DisplayName }
-       
-       
-      #The code below is commented out but can be used to see the resource group name and the sql statements executed
-       # Write-Host "$($Server.ResourceGroupName)" 
-       #Write-Output "Invoke-Sqlcmd -ServerInstance $($ServerInstancename) -Database 'master' -Query $query -AccessToken $($accessToken)" | Out-File -FilePath "C:\Temp\Output.csv" 
-        Write-host "ResourceGroupName: $($Server.ResourceGroupName) SQLDB ServerName: $($ServerInstancename) "    # | Out-File -FilePath "C:\Temp\Output.csv" 
-               
-                                                                }
-       
-           
-                            }
-            }
-  }  
+$query = ""
+$query = " IF NOT EXISTS (SELECT 1 FROM dbo.Targets WHERE servername = '$ServerInstancename' and DBNAME = '$DBNAME' )
+            BEGIN
+                    INSERT INTO dbo.Targets (AdminName, ResourceGroupName, servername, servertype, DeploymentModel, ServiceTier, DBNAME, contactperson, dbwatchername, enrolled, RegistrationDate )
+                    VALUES ('$AdminName', '$ResourceGroupName', '$ServerInstancename', 'Azure SQL Database', '$DeploymentModel', '$ServiceTier', '$DBNAME', 'DBA@BigCorp.com', '$DBWatcherName', 'N' , GETDATE() ); 
+            END
+                    ELSE
+            BEGIN
+                    UPDATE dbo.Targets
+                           SET AdminName = '$AdminName', ResourceGroupName = '$ResourceGroupName', DeploymentModel = '$DeploymentModel', ServiceTier = '$ServiceTier'
+                    WHERE servername = '$ServerInstancename' AND DBNAME = '$DBNAME';
 
+            END "
+
+
+
+       Invoke-Sqlcmd -ServerInstance $MainServerName -Database $MaintDBName -AccessToken $accessToken -Query $query
+       Invoke-Sqlcmd -ServerInstance $MainServerName -Database $MaintDBName -AccessToken $accessToken -Query 'GO' 
+       Write-host ""
+       Write-host $query
+       
+       }
+     }
+   }  
   
+  }
+ 
