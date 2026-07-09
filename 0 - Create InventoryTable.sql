d0aec6a8ee7526f1d227ea1ff3af32d100b1c27f
bob0900 <bob.cheatham@gmail.com>
2026-07-09 10:30:09 -0400

    Add files via upload
    SQL Script used to store all SQL DB and SQL MI server names

---
+++ 0 - Create InventoryTable.sql
@@ -0,0 +1,32 @@
+/****** Object:  Table [dbo].[Targets]    Script Date: 7/9/2026 10:19:58 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Targets](
+	[ServerID] [int] IDENTITY(1,1) NOT NULL,
+	[servername] [nvarchar](80) NOT NULL,
+	[servertype] [nvarchar](80) NOT NULL,
+	[contactperson] [varchar](100) NOT NULL,
+	[dbwatchername] [varchar](80) NULL,
+	[enrolled] [varchar](1) NULL,
+	[RegistrationDate] [datetime2](7) NULL,
+	[DBNAME] [varchar](255) NULL,
+	[ServiceTier] [varchar](255) NULL,
+	[ComputeTier] [varchar](255) NULL,
+	[DeploymentModel] [varchar](255) NULL,
+	[AdminName] [varchar](75) NULL,
+	[ResourceGroupName] [varchar](255) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ServerID] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Targets] ADD  DEFAULT (getdate()) FOR [RegistrationDate]
+GO
+
+
