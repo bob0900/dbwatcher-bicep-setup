622f827b1dadbb88c0d8aca8b71f5051582a5615
bob0900 <bob.cheatham@gmail.com>
2026-07-10 17:32:48 -0400

    Add files via upload
    Add SQL DB as inventory source

--- 1 Permissions - Add DBwatcher-EntraID to SQLDB Instances.ps1
+++ 1 Permissions - Add DBwatcher-EntraID to SQLDB Instances.ps1
@@ -1,5 +1,6 @@
 ﻿#Install-Module az.accounts -MinimumVersion 5.5.0 -Force -AllowClobber
-#Import-Module Az.Accounts -RequiredVersion 5.5.0 -Force
+#Update-Module -Name Az.Accounts -Force -RequiredVersion 5.5.1
+#update-Module az.accounts -requiredversion 5.5.1 -Force 
 #Install-module az.sql -RequiredVersion 7.0.0 -Force -AllowClobber
 #Import-Module az.sql -RequiredVersion 7.0.0 -force
 #Install-Module -Name SqlServer -RequiredVersion 22.4.5.1 -Force -AllowClobber
@@ -8,7 +9,7 @@
 
 #Connect-AzAccount -UseDeviceAuthentication
 
-$DBWatcherName = "DBWatcherFullName"
+$DBWatcherName = "DBWatcher-Name-Goes-Here"
 
 $query = "IF NOT EXISTS (SELECT 1 FROM sys.server_principals WHERE name = '$($DBWatcherName)')
 BEGIN CREATE LOGIN [$($DBWatcherName)] FROM EXTERNAL PROVIDER; END
@@ -16,59 +17,46 @@ ALTER SERVER ROLE ##MS_ServerPerformanceStateReader## ADD MEMBER [$($DBWatcherNa
 ALTER SERVER ROLE ##MS_DefinitionReader## ADD MEMBER [$($DBWatcherName)];
 ALTER SERVER ROLE ##MS_DatabaseConnector## ADD MEMBER [$($DBWatcherName)];"
 
+
 # Get the access token for Azure SQL MI
 $accessToken = (Get-AzAccessToken -ResourceUrl "https://database.windows.net").Token
+
+# Define inventory database details 
+$DBMaintServer   = "sqldb-maintenance-robertc.database.windows.net"
+$Database = "dbMaintenance"
+$SqlQuery = "SELECT  distinct a.servername FROM (select * from [dbo].[Targets] where ServiceTier not in ('DataWareHouse') ) a  WHERE a.AdminName Like '%' AND a.ResourceGroupName Like '%' "
+
+
+# Execute the inventory resluts query and store the results in an array
+$Rows = Invoke-Sqlcmd -ServerInstance $DBMaintServer -Database $Database -Query $SqlQuery -AccessToken $accessToken
+
+
+#Update SQL Inventory table with DB-WatcherName
+$SQLUpdate = "UPDATE dbo.Targets
+                   SET dbwatchername = '$DBWatcherName'
+              WHERE servername = '$ServerInstancename' AND ServiceTier != 'DataWarehouse' "
 
 
 $subsWithSqlServers = foreach ($sub in Get-AzSubscription) 
 {
-  Set-AzContext -SubscriptionId $sub.Id | Out-Null
-  $servers = Get-AzSqlServer -ErrorAction SilentlyContinue
-  
-}  
 
-$sqlServers = Get-AzSqlServer -ErrorAction SilentlyContinue
-     foreach ($server in $sqlServers) 
-     {
+Set-AzContext -SubscriptionId $sub.Id | Out-Null
 
-        #Write-Host "  SQL Server: $($server.ServerName) - Resource Group: $($server.ResourceGroupName)"
-        # List SQL Databases in this server
 
-        $ServerInstancename = "$($server.FullyQualifiedDomainName)"
-        #Write-Host $ServerInstancename
-        #Write-Host "   InstaneName - $($ServerInstancename) - SQL Database: $($db.DatabaseName) - Database Edition: $($db.Edition)"
+foreach ($Row in $Rows) 
+{
+        # Access individual columns using dot-property notation
+        $SQLDBName = $Row.ServerName
+    
+    
+        Write-Host ""
 
-       #Invoke-Sqlcmd -ServerInstance $ServerInstancename -Database 'master' -Query $query -AccessToken $accessToken 
-                  
-                                       
-    if ($server.ResourceGroupName -like "**") 
-    {
-        
-        $admin = Get-AzSqlServerActiveDirectoryAdministrator `
-                    -ResourceGroupName $server.ResourceGroupName `
-                    -ServerName $server.ServerName `
-                    -ErrorAction SilentlyContinue
-
-      if ($admin -and $admin.ObjectId -ne "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx") 
-           {
-            [PSCustomObject]@{
-                SubscriptionId = [string]$sub.Id
-                ServerName     = [string]$server.ServerName
-                ResourceGroup  = [string]$server.ResourceGroupName
-                AdminObjectId  = [string]$admin.ObjectId
-                AdminName      = [string]$admin.DisplayName }
-                 
-        #Invoke-Sqlcmd -ServerInstance $ServerInstancename -Database 'master' -Query $query -AccessToken $accessToken 
-        #Write-Output "ServerName: $($ServerInstancename) Query = $($query) "
-           }
-
-        #Write-Output "ResourceGroupName: $($Server.ResourceGroupName) ServerName: $($ServerInstancename) Permissions Onboarded" | Out-File -FilePath "C:\Temp\Output.csv"      
-        Write-Output Invoke-Sqlcmd -ServerInstance $($ServerInstancename) -Database 'master' -Query $query -AccessToken $($accessToken)
-    }
+                
+        #Write-Host "Invoke-Sqlcmd -ServerInstance $($SQLDBName) -Database 'master' -Query $query -AccessToken $($accessToken)"
+        Invoke-Sqlcmd -ServerInstance $($SQLDBName) -Database 'master' -Query $query -AccessToken $($accessToken)
+}
               
-       #The code below is commented out but can be used to see the resource group name and the sql statements executed
-       # Write-Host "$($Server.ResourceGroupName)" 
         
-        
-               
-      }
+}
+
+    
